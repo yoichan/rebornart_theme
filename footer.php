0ebc9f18905d9cea338351a0ce9ded286c6e9670
Yayoi Ito <yayoi.t.ito@gmail.com>
2017-03-12 11:45:17 +0900

    スムーススクロールするようにした

--- footer.php
+++ footer.php
@@ -138,5 +138,18 @@
 
 <?php wp_footer(); ?>
 
+<script type="text/javascript">
+    (function($){ $(function(){
+        $('a[href^="#"]').click(function(){
+            var speed = 500;
+            var href= $(this).attr("href");
+            var target = $(href == "#" || href == "" ? 'html' : href);
+            var position = target.offset().top;
+            $("html, body").animate({scrollTop:position}, speed, "swing");
+            return false;
+        });
+    });})(jQuery);
+</script>
+
 </body>
 </html>
